533b8a2a389a6809b06999b02b935c2f6f728ade
johanneslepsius <johannes.lepsius@chasacademy.se>
2021-01-07 22:17:15 +0100

    minor fix

--- public/includes/catvalidation.inc.php
+++ public/includes/catvalidation.inc.php
@@ -2,10 +2,10 @@
 var_dump($_POST);
 $tasktext = $_POST["tasktext"];
 $catcheck1 = stripos($tasktext, "cat");
-// preg_match("/^[0-9]*$/", $catcheck1)
 echo $tasktext;
 
 if (!is_numeric($catcheck1)) {
+    //this is triggered if you try to create a task not involving the cat
     header("location: ../index.php?cat=sad");
 } else {
     header("location: ../includes/handleinput.inc.php?title={$_POST['title']}&tasktext={$_POST['tasktext']}");
